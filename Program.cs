7ca664b63c752726e57fe72d33f60bae1e33c1e7
vlad drabovskyi <drabovskyi.vladyslav1121@vu.cdu.edu.ua>
2021-12-15 22:55:00 +0200

    додано підрахунок суми чисел і виведення результату

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace мтзгит
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10={0}",x+y);
             Console.ReadKey();
 
         }
